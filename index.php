fc6cf1434a792499ced1d5954a1a85be6cd577cd
Aidan Hahn <aidan@aidanis.online>
2019-05-30 22:33:37 -0700

    include bootstrap dependancy

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ function escape ($data) {
 <html>
 <head>
   <title>Missing Children Database Portal</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
 
