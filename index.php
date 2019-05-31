75709c59d359cff5dca80a5c2d522eff277c5a9d
Aidan Hahn <aidan@aidanis.online>
2019-05-30 20:42:14 -0700

    theoretically this will display query results as a table

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
+$selectQuery = ""; // TODO: this
+$tableColumns = array(); //TODO: this
+
 $serverName = "localhost";
 $username = "root";
 $password = "uwu";
-$conn = new mysqli($serverName, $username, $password);
+$conn = new mysqli($serverName, $username, $password, "Missing Children");
 
 if ($conn->connect_error) {
-	die("Failed to connect to database: ".$conn->connect_error);
+        die("Failed to connect to database: ".$conn->connect_error);
 }
+
+$results = $conn->query($selectQuery);
+
+mysqli_close($conn);
 ?>
 <html>
 <head>
@@ -15,6 +22,29 @@ if ($conn->connect_error) {
 <link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
 </head>
 <body>
-<h1>Short code <?=?></h1>
+<h1>Data in table (TODO: name table)</h1>
+<table class="table">
+  <thead>
+    <tr>
+<?php
+foreach ($tableColumns as &$column) {
+	echo "<th>".$column."</th>";
+}
+?> 
+    </tr>
+  </thead>
+  <tbody>
+<?php
+while ($row = $results->fetch_assoc()) {
+echo "<tr>";
+foreach ($row as $key => $value) {
+	echo "<td>".$key."</td>";
+}
+echo "</tr>";
+}
+?>     
+  </tbody>
+</table>
 </body>
 </html>
+
