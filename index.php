5c3c8574be6b218598ff63f9a0b8e30559bed63d
Aidan Hahn <aidan@aidanis.online>
2019-05-30 21:54:42 -0700

    search form

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$selectQuery = "SELECT * FROM MissingCase"; 
+$selectQuery = "SELECT * FROM MissingCase ORDER BY childID"; 
 $tableColumns = array("Case ID", "Potential Amber Alert", "Abduction", "Is in NCIC DB", "Report Type", "Search ID", "Amber Alert ID", "Child ID", "Media Release Request ID", "Private Detective ID", "Suspect ID");
 
 $serverName = "vergil.u.washington.edu";
@@ -36,7 +36,17 @@ div {
 
 <body>
 <div>
-<h1>Missing Child Cases</h1>
+<h1 style="align:center;">Missing Child Cases</h1>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+Value: <input type="text" name="val">
+<select name="field">
+  <option value="childID">Child ID</option>
+  <option value="suspectID">Suspect ID</option>
+</select><br>
+<input type="submit">
+</form>
+
 <table class="table">
   <thead>
     <tr>
@@ -52,7 +62,12 @@ div {
   while ($row = $results->fetch_assoc()) {
     echo "<tr>";
     foreach ($row as $key => $value) {
-      echo "<td>".$value."</td>";
+      if ($key = "caseID") {
+        echo "<td><a href=\"CSS475/query.php?id=".$value."\">".$value."</a></td>";
+
+      } else {
+        echo "<td>".$value."</td>";
+      }
     }
     echo "</tr>";
   }
