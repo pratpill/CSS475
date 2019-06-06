a981fa478d21c031c31bb1b6c5e9ee06ada1e8e4
yashinator99 <43225047+yashinator99@users.noreply.github.com>
2019-06-05 23:29:45 -0700

    Update query.php

--- query.php
+++ query.php
@@ -36,24 +36,13 @@ function getInfo($caseID, $conn)
     $results = $conn->query($selectQuery);
     return $results;
 }
-/*function getVolunteerInfo($caseId, $conn)
-{
-    $selectQuery = "SELECT * FROM (SELECT * FROM MissingCase WHERE caseID = ".$caseID.") c".
-    " INNER JOIN (SELECT * FROM SearchVolunteer) sv ON sv.searchID = c.searchID".
-    " INNER JOIN (SELECT * FROM Volunteer) v ON v.ID = sv.searchVolunteerID";
-    $vol_results = $conn->query($selectQuery);
-    return $vol_results;
 
-} */
 $id_val = $_GET["id"];
-#echo("Child Id=".$id_val);
-#echo("Connecting to database");
+
 $conn = getConnToDb();
-#echo("Connected to database");
-#echo("Get Child Info");
+
 $results = getInfo($id_val, $conn);
-//$vol_results = getVolunteerInfo($id_val, $conn);
-#echo("close connection");
+
 closeConnection($conn);
 ?>
 <h1>Missing Child Detailed Page</h1>
@@ -81,11 +70,8 @@ closeConnection($conn);
     <p style="padding-right: 10px;"></p>
     <p style="padding-right: 40px;">
         <b>Address:</b>
-        <input type="text" size="5" value=<?php printf('%s',$row[40])?> disabled />
-        <input type="text" size="20" value=
-                <?php
-                     echo($row[41]);
-                 ?>
+        <input type="text" size="5" value="<?php printf('%s',$row[40])?>" disabled />
+        <input type="text" size="20" value="<?php echo($row[41]); ?>"
                 disabled />
         <input type="text" size="18" value=<?php printf('%s',$row[38])?> disabled />
         <input type="text" size="18" value=<?php printf('%s',$row[39])?> disabled />
@@ -151,63 +137,13 @@ closeConnection($conn);
     </p>
     <p style="padding-right: 40px;">
         <b style="padding-right: 100px;">Descprition</b>
-        <input type="text" size="27" value=<?php printf('%s',$row[57])?> disabled />
+        <input type="text" size="27" value="<?php printf('%s',$row[57])?>" disabled />
     </p>
     <p style="padding-right: 40px;">
         <b style="padding-right: 38px;">Reason Of Suspicion</b>
-        <input type="text" size="27" value=<?php printf('%s',$row[58])?> disabled />
+        <input type="text" size="27" value="<?php printf('%s',$row[58])?>" disabled />
     </p>
-<!--<h3>Volunteers</h3>
-<?php
-        if ($vol_results->num_rows > 0) {
-?>
-<table>
-<tr>
-   <td> First Name </td>
-   <td> Last  Name </td>
-</tr>
-<?php
-           while ($vrow = $vol_results->fetch_assoc()) {
 
-             echo "<tr>";
-
-             foreach ($vrow as $key => $value) {
-
-               if (($key == "FirstName") || ($key == "LastName"))  {
-
-                 echo "<td> echo($value)</td>";
-               }
-
-   	     }
-   
-             echo "</tr>";
-           }
-?>
-</table>
-<?php
-        }
-        else
-        {
-          echo "No Volunteers";
-        }
-?>
-<hr>
-<h2>Raw Data</h2>
- <?php
-            #echo ($row["ChildID"]);
-            foreach ($row as $key => $value) {
-                echo("<p>");
-                echo($key.": ");
-                echo($value." ");
-                echo("</p>");
-            }
-
-
-        }
-        else
-        {
-            echo("no results have returned");
-        }
-?>   -->
 </body>
 </html>
+
