90f5f24c1ef8d63c6d257745a365c766674ca63a
Aidan Hahn <aidan@aidanis.online>
2019-06-06 23:17:27 -0700

    added setting the MissingCase PublicationRequestID

--- update.php
+++ update.php
@@ -1,6 +1,29 @@
 <?php
+$serverName = "vergil.u.washington.edu";
+$username = "root";
+$password = "uwu";
+$conn = new mysqli($serverName, $username, $password, "Missing Children", 8448);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id_val = escape($_POST["id"]);
+    if($conn->query("SELECT * FROM MissingCase WHERE caseID = ".$id_val)->num_rows() 
+			< 1) {
+			die("Bad Case ID, try again from the beginning");			
+    }
+
+} else {
+    $id_val = escape($_GET["id"]);
+    if($conn->query("SELECT * FROM MissingCase WHERE caseID = ".$id_val)->num_rows() 
+			< 1) {
+			die("Bad Case ID, try again from the beginning");			
+    }
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $updateQuery = "INSERT INTO PublicationRequest(signature, signatureDate, printName, relationshipCode, requesterphoneNo) VALUES (";
+  $getPubReqPrimKeyQueryHead = "SELECT requestID FROM PublicationRequest WHERE signatureDate = ";
+  $setPubReqInMissingCaseQueryHead = "UPDATE MissingCase SET requestID = ";
+  $setPubReqInMissingCaseQueryTail = " WHERE caseId = ";
   $signed = $_POST["signed"] ? "1" : "0";
   $date = htmlspecialchars($_POST["date"]);
   $name = escape($_POST["name"]);
@@ -8,17 +31,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $phone = escape($_POST["phone"]);
 
   // TODO: validate here.
-  
-  $serverName = "vergil.u.washington.edu";
-  $username = "root";
-  $password = "uwu";
-  $conn = new mysqli($serverName, $username, $password, "Missing Children", 8448);
 
   if ($conn->connect_error) {
     die("Failed to connect to database: ".$conn->connect_error);
   }
 
-  $results = $conn->query($updateQuery.$signed.", ".$date.", ".$name.", ".$relation.", ".$date);
+  $results = $conn->query($updateQuery.$signed.", ".$date.", ".$name.", ".$relation.", ".$phone);
+  $primKey = $conn->query($getPubReqPrimKeyQueryHead.$date." AND printName = ".$name." AND relationshipCode = ".
+			$relation." AND requesterphoneNo = ".$phone).fetch_row()[0];
+  $conn->query($setPubReqInMissingCaseQueryHead.$id_val.$setPubReqInMissingCaseQueryTail.$primKey);
+
   mysqli_close($conn);
 }
 
