0638d1c3534e5184b94d6b52f765cb1703b95839
yashinator99 <43225047+yashinator99@users.noreply.github.com>
2019-06-09 13:47:41 -0700

    Update query.php

--- query.php
+++ query.php
@@ -92,24 +92,24 @@ closeConnection($conn);
 <form>
     <p style="padding-right: 132px;">
         <b style="padding-right: 30px;">First Name:</b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[22]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[22]) ?>" disabled />
         <b style="padding-left: 30px;">Last Name:</b>
 	<b style="padding-left: 25px;" ></b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[23]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[23]) ?>" disabled />
     </p>
     <p style="padding-right: 40px;">
-        <b>Gender:</b> <input type="text" value=<?php echo($row[21]) ?> disabled />
-        <b>Race:</b> <input type="text" value=<?php echo($row[20]) ?> disabled />
-        <b>Last Seen:</b> <input type="text" value=<?php echo($row[16]) ?> disabled />
+        <b>Gender:</b> <input type="text" value="<?php echo($row[21]) ?>" disabled />
+        <b>Race:</b> <input type="text" value="<?php echo($row[20]) ?>" disabled />
+        <b>Last Seen:</b> <input type="text" value="<?php echo($row[16]) ?>" disabled />
     </p>
     <p style="padding-right: 10px;"></p>
     <p style="padding-right: 40px;">
         <b>Address:</b>
         <input type="text" size="5" value="<?php printf('%s',$row[40])?>" disabled />
         <input type="text" size="20" value="<?php echo($row[41]); ?>" disabled />
-        <input type="text" size="18" value=<?php printf('%s',$row[38])?> disabled />
-        <input type="text" size="18" value=<?php printf('%s',$row[39])?> disabled />
-        <input type="text" size="5" value=<?php printf('%s',$row[37])?> disabled />
+        <input type="text" size="18" value="<?php printf('%s',$row[38])?>" disabled />
+        <input type="text" size="18" value="<?php printf('%s',$row[39])?>" disabled />
+        <input type="text" size="5" value="<?php printf('%s',$row[37])?>" disabled />
 
     </p>
 </form>
@@ -117,10 +117,10 @@ closeConnection($conn);
 <form>
     <p style="padding-right: 132px;">
         <b style="padding-right: 30px;">First Name:</b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[48]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[48]) ?>" disabled />
         <b style="padding-left: 30px;">Last Name:</b>
 	<b style="padding-left: 25px;" ></b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[49]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[49]) ?>" disabled />
     </p>
     <p style="padding-right: 40px;">
         <b style="padding-right: 62px;">Phone:</b>
@@ -133,7 +133,7 @@ closeConnection($conn);
 <form>
     <p style="padding-right: 132px;">
         <b style="padding-right: 30px;">Alert Date:</b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[16]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[16]) ?>" disabled />
         <b style="padding-left: 30px;">Alert Resolved:</b>
 	<b style="padding-left: 25px;" ></b>
 	<input type="text" style="padding-right: 40px;" value=
@@ -154,20 +154,20 @@ closeConnection($conn);
 <form>
     <p style="padding-right: 132px;">
         <b style="padding-right: 30px;">First Name:</b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[43]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[43]) ?>" disabled />
         <b style="padding-left: 30px;">Last Name:</b>
 	<b style="padding-left: 25px;" ></b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[44]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[44]) ?>" disabled />
     </p>
     <p style="padding-right: 40px;">
 </form>
 <h3>Suspect</h3>
     <p style="padding-right: 132px;">
         <b style="padding-right: 30px;">First Name:</b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[53]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[53]) ?>" disabled />
         <b style="padding-left: 30px;">Last Name:</b>
 	<b style="padding-left: 25px;" ></b>
-	<input type="text" style="padding-right: 40px;" value=<?php echo($row[54]) ?> disabled />
+	<input type="text" style="padding-right: 40px;" value="<?php echo($row[54]) ?>" disabled />
     </p>
     <p style="padding-right: 40px;">
         <b style="padding-right: 100px;">Descprition</b>
